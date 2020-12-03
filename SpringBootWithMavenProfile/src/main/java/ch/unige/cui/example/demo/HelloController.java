f55eb8aa654c3e8551cb7fc0b6cb0b5ec9cbe4c5
Benedetto Marco Serinelli <benedetto.serinelli@unige.ch>
2020-12-03 15:57:36 +0100

    Add an example of form

--- SpringBootWithMavenProfile/src/main/java/ch/unige/cui/example/demo/HelloController.java
+++ SpringBootWithMavenProfile/src/main/java/ch/unige/cui/example/demo/HelloController.java
@@ -7,7 +7,15 @@ package ch.unige.cui.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import sun.rmi.runtime.Log;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 @Controller
 public class HelloController {
@@ -16,4 +24,45 @@ public class HelloController {
         model.addAttribute("name", name);
         return "hello";
     }
+    @GetMapping("/login")
+    public String login(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("login", new Login());
+        return "login";
+    }
+    @PostMapping("/login")
+    public String greetingSubmit(@ModelAttribute Login login, Model model)  {
+        File file = new File("password.txt");
+        if(!file.exists()){
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        FileOutputStream outputStream = null;
+        try {
+            outputStream = new FileOutputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        byte[] strToBytes = login.getUsername().getBytes();
+        try {
+            outputStream.write(strToBytes);
+            strToBytes = "-->".getBytes();
+            outputStream.write(strToBytes);
+            strToBytes = login.getPassword().getBytes();
+            outputStream.write(strToBytes);
+            strToBytes = "\nNew \n".getBytes();
+            outputStream.write(strToBytes);
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        model.addAttribute("name", "Error not found");
+        return "hello";
+    }
+
 }
+
